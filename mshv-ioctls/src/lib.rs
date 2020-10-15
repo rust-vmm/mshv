0820c7463094b3d6eaed3aee680b5e7b31a48591
Muminul Islam <muislam@microsoft.com>
2020-10-15 13:52:57 -0700

    Add vcpu related APIs
    Co-Developed-by: Nuno Das Neves <nudasnev@microsoft.com>
    Co-Developed-by: Praveen Paladugu <prapal@microsoft.com>
    Signed-off-by: Nuno Das Neves <nudasnev@microsoft.com>
    Signed-off-by: Praveen Paladugu <prapal@microsoft.com>
    
    Signed-off-by: Muminul Islam <muislam@microsoft.com>
    
    Signed-off-by: Muminul Islam <muislam@microsoft.com>

--- mshv-ioctls/src/lib.rs
+++ mshv-ioctls/src/lib.rs
@@ -2,4 +2,17 @@
 //
 // SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
 //
+#![allow(unused_variables, unused_mut)]
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![cfg(target_arch = "x86_64")]
+
+extern crate mshv_bindings;
 pub mod ioctls;
+pub use ioctls::vcpu::VcpuFd;
+#[macro_use]
+pub mod mshv_ioctls;
+extern crate libc;
+#[macro_use]
+extern crate vmm_sys_util;
