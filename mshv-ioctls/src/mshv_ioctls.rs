0ae29883df796bb8bcb409500e9544d8a2822583
Muminul Islam <muislam@microsoft.com>
2020-10-15 14:05:16 -0700

    Add VM related IOCTLs
    Co-Developed-by: Nuno Das Neves <nudasnev@microsoft.com>
    Signed-off-by: Nuno Das Neves <nudasnev@microsoft.com>
    
    Signed-off-by: Muminul Islam <muislam@microsoft.com>

--- mshv-ioctls/src/mshv_ioctls.rs
+++ mshv-ioctls/src/mshv_ioctls.rs
@@ -3,8 +3,46 @@
 // SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
 //
 use mshv_bindings::*;
+
+ioctl_iow_nr!(MSHV_CREATE_VP, MSHV_IOCTL, 0x04, mshv_create_vp);
 ioctl_iowr_nr!(MSHV_GET_VP_REGISTERS, MSHV_IOCTL, 0x05, mshv_vp_registers);
 ioctl_iow_nr!(MSHV_SET_VP_REGISTERS, MSHV_IOCTL, 0x06, mshv_vp_registers);
 ioctl_ior_nr!(MSHV_RUN_VP, MSHV_IOCTL, 0x07, hv_message);
 ioctl_iowr_nr!(MSHV_GET_VP_STATE, MSHV_IOCTL, 0x0A, mshv_vp_state);
 ioctl_iowr_nr!(MSHV_SET_VP_STATE, MSHV_IOCTL, 0x0B, mshv_vp_state);
+ioctl_iow_nr!(
+    HV_SET_PARTITION_PROPERTY,
+    MSHV_IOCTL,
+    0x0C,
+    mshv_partition_property
+);
+ioctl_iowr_nr!(
+    HV_GET_PARTITION_PROPERTY,
+    MSHV_IOCTL,
+    0x0D,
+    mshv_partition_property
+);
+ioctl_iow_nr!(
+    MSHV_MAP_GUEST_MEMORY,
+    MSHV_IOCTL,
+    0x02,
+    mshv_user_mem_region
+);
+ioctl_iow_nr!(
+    MSHV_UNMAP_GUEST_MEMORY,
+    MSHV_IOCTL,
+    0x03,
+    mshv_user_mem_region
+);
+ioctl_iow_nr!(
+    MSHV_INSTALL_INTERCEPT,
+    MSHV_IOCTL,
+    0x08,
+    mshv_install_intercept
+);
+ioctl_iow_nr!(
+    MSHV_ASSERT_INTERRUPT,
+    MSHV_IOCTL,
+    0x09,
+    mshv_assert_interrupt
+);
