ab08b443c5d9e5c8a29c81d4570efe939ee3a6ab
Muminul Islam <muislam@microsoft.com>
2020-10-15 12:22:48 -0700

    Add rust equivalent bindings code
    Co-Developed-by: Nuno Das Neves <nudasnev@microsoft.com>
    
    Signed-off-by: Nuno Das Neves <nudasnev@microsoft.com>
    Signed-off-by: Muminul Islam <muislam@microsoft.com>

--- mshv-bindings/src/lib.rs
+++ mshv-bindings/src/lib.rs
@@ -2,3 +2,10 @@
 //
 // SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
 //
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![cfg(target_arch = "x86_64")]
+
+pub mod bindings;
+pub use bindings::*;
