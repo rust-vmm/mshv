f170216f8b372db0b6da38db842cf1d60f9cb74c
Muminul Islam <muislam@microsoft.com>
2021-06-30 10:42:11 -0700

    mhsv-{bindings, ioctls}: Generate new bindings
    Updated bindings has get_gpa_access_states API
    needed for get_dirty_log. This patch also fixes
    translate_gva ioctls call
    
    Signed-off-by: Muminul Islam <muislam@microsoft.com>

--- mshv-ioctls/src/mshv_ioctls.rs
+++ mshv-ioctls/src/mshv_ioctls.rs
@@ -55,9 +55,4 @@ ioctl_iow_nr!(
     0x09,
     mshv_assert_interrupt
 );
-ioctl_iowr_nr!(
-    MSHV_VP_TRANSLATE_GVA,
-    MSHV_IOCTL,
-    0x0E,
-    mshv_vp_translate_gva
-);
+ioctl_iowr_nr!(MSHV_VP_TRANSLATE_GVA, MSHV_IOCTL, 0x0E, mshv_translate_gva);
