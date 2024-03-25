1dc2f795714dcf8122b3229bb1a93836677bd326
Jinank Jain <jinankjain@microsoft.com>
2024-03-26 00:18:47 +0530

    mshv-ioctls: Add support for aarch64
    Remove conditional compilation for x86_64.
    
    Signed-off-by: Jinank Jain <jinankjain@microsoft.com>

--- mshv-ioctls/src/lib.rs
+++ mshv-ioctls/src/lib.rs
@@ -2,7 +2,6 @@
 //
 // SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
 //
-#![cfg(target_arch = "x86_64")]
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
