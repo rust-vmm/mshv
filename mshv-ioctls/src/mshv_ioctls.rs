21985a348104942c5e37be83c7a5fac883d89a61
Nuno Das Neves <nudasnev@microsoft.com>
2024-03-28 19:54:47 +0000

    ioctls: Export the raw ioctl numbers
    To allow seccomp rules to use them instead of hard-coding them.
    
    Disable clippy doc requirement in mshv_ioctls.rs
    
    Signed-off-by: Nuno Das Neves <nudasnev@microsoft.com>

--- mshv-ioctls/src/mshv_ioctls.rs
+++ mshv-ioctls/src/mshv_ioctls.rs
@@ -2,6 +2,8 @@
 //
 // SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
 //
+#![allow(missing_docs)]
+
 use mshv_bindings::*;
 ioctl_iow_nr!(MSHV_CREATE_VP, MSHV_IOCTL, 0x04, mshv_create_vp);
 ioctl_iowr_nr!(MSHV_GET_VP_REGISTERS, MSHV_IOCTL, 0x05, mshv_vp_registers);
