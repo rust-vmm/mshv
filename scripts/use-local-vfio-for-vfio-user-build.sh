533b726e31a7df0ef6d30d5859c11375fa294ea0
Muminul Islam <muislam@microsoft.com>
2025-04-18 17:58:22 -0700

    scripts: script to use local vfio for vfio-user
    Signed-off-by: Muminul Islam <muislam@microsoft.com>

---
+++ scripts/use-local-vfio-for-vfio-user-build.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+filename=./Cargo.toml
+
+line=$(grep -n "vfio-bindings = { git =" $filename | tail -n1 | cut -f1 -d:)
+if [ -z $line ]; then
+        echo "vfio-bindings not found in the Cargo.toml file"
+        exit 1
+fi
+sed -i "$line i vfio-bindings = { path = \"../vfio/crates/vfio-bindings\" }" $filename
+line=$((line+1))
+sed -i "${line}d" $filename
